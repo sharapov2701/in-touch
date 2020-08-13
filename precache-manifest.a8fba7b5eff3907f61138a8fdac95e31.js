43482cf0d8ad9e3377811a61d544b2eee836044f
sharapov2701 <50914537+sharapov2701@users.noreply.github.com>
2020-08-13 03:31:55 +0300

    Updates

---
+++ precache-manifest.a8fba7b5eff3907f61138a8fdac95e31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "585e91664ac2594467a71b7d5a5d5b1a",
+    "url": "/in-touch/index.html"
+  },
+  {
+    "revision": "6384c54db1106b48aff7",
+    "url": "/in-touch/static/css/main.a003c1d1.chunk.css"
+  },
+  {
+    "revision": "afcac980f9506acd7ce3",
+    "url": "/in-touch/static/js/2.62689780.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/in-touch/static/js/2.62689780.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6384c54db1106b48aff7",
+    "url": "/in-touch/static/js/main.beecfaab.chunk.js"
+  },
+  {
+    "revision": "3376df6ef664ea3f929b",
+    "url": "/in-touch/static/js/runtime-main.9d646652.js"
+  }
+]);
